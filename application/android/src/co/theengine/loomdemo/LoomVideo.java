55a5de533588c827a891cb3815fd3900529dc555
Luke Lamothe <luke.lamothe@gmail.com>
2013-10-19 13:24:32 +0200

    Added commenting for doc generation for the Video class.  Attempted to fix the dodgy video gravity sliding issues on Android… not really successful though :(

--- application/android/src/co/theengine/loomdemo/LoomVideo.java
+++ application/android/src/co/theengine/loomdemo/LoomVideo.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.widget.RelativeLayout;
 import android.widget.VideoView;
 import android.widget.MediaController;
@@ -19,6 +20,7 @@ import java.util.Date;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.view.Gravity;
 import android.graphics.Color;
 
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
@@ -40,12 +42,15 @@ public class LoomVideo
     private static ViewGroup    _rootView = null;
     private static VideoView    _videoView = null;
     private static Activity     _context = null;
+    private static String       _videoFile = null;
     private static int          _controlMode = Controls_Show;
+    private static int          _scaleMode = Scale_None;
+    private static int          _bgColor = Color.TRANSPARENT;
 
 
 
     ///class to implement the listerer interfaces to act upon video states
-    public static class VideoListeners implements OnCompletionListener, OnErrorListener, OnTouchListener
+    public static class VideoListeners implements OnCompletionListener, OnErrorListener, OnTouchListener, OnPreparedListener
     {
         @Override
         public void onCompletion(MediaPlayer mp) 
@@ -110,7 +115,24 @@ public class LoomVideo
             ///swallow the event if controls are hidden
             return (_controlMode == Controls_Hide) ? true : false;
         }
-    }
+
+
+        @Override
+        public void onPrepared(MediaPlayer mp)
+        {
+            _videoView.requestFocus();
+
+            ///set the layout of the video to the user specs
+            _videoView.setLayoutParams(getLayout(mp.getVideoWidth(), mp.getVideoHeight()));  
+            _videoView.invalidate();
+            _videoView.requestLayout();
+
+            ///set root color for outside of the video play area to make it visible (if desired) now
+            _rootView.setVisibility(View.VISIBLE);
+            _rootView.setBackgroundColor(_bgColor);
+        }
+    } 
+
 
 
 
@@ -127,12 +149,14 @@ public class LoomVideo
         _videoView.setVisibility(View.VISIBLE);
         _videoView.setBackgroundColor(Color.TRANSPARENT);
         _videoView.setKeepScreenOn(true);
+        _videoView.setId(1234);            
 
         ///set up listeners to the view to react to events
         VideoListeners videoListeners = new VideoListeners();
         _videoView.setOnCompletionListener(videoListeners);
         _videoView.setOnErrorListener(videoListeners);
         _videoView.setOnTouchListener(videoListeners);
+        _videoView.setOnPreparedListener(videoListeners);
     }
 
 
@@ -163,34 +187,14 @@ public class LoomVideo
     {
         Log.d("Loom", "Video Play Fullscreen: " + file + " " + scaleMode + " " + controlMode + " " + bgColor);
 
-///TODO: Scale_None
-        ///set layout of the video view based on the layout flags
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, 
-                                                                                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+        ///store some vars
+        _videoFile = file;
+        _scaleMode = scaleMode;
+        _controlMode = controlMode;
+        _bgColor = bgColor;
 
-        switch(scaleMode)
-        {
-            case Scale_None:
-                layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-                break;
-            case Scale_Fill:
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-                break;
-            case Scale_FitAspect:
-                layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-                break;
-        }
-        // _videoView.setLayoutParams(layoutParams);
-
-        ///add video view to the parent view with the desired layout
-        _rootView.addView(_videoView, layoutParams);
-        _videoView.requestFocus();
 
         ///if specified, create media controller and link it with the video view
-        _controlMode = controlMode;
         switch(_controlMode)
         {
             case Controls_Show:
@@ -207,20 +211,61 @@ public class LoomVideo
                 break;
         }
 
-        ///set root color for outside of the video play area
-        _rootView.setBackgroundColor(bgColor);
+        ///ignore gravity to try and stop the stupid retarded video view from stupid retarded sliding
+        ((RelativeLayout)_rootView).setIgnoreGravity(_videoView.getId());
+
+        ///hide the root until the video is ready to play
+        _rootView.setVisibility(View.INVISIBLE);
+        _rootView.setBackgroundColor(Color.TRANSPARENT);
 
         ///send the video URI to the video view
         ///NOTE: Both of these methods, either with the raw ID or /raw/filename_noext seem to work
-        // int videoResourceID = _context.getResources().getIdentifier(file, "raw", _context.getPackageName());
-        // Uri videoUri = Uri.parse("android.resource://" + _context.getPackageName() + "/" + videoResourceID);        
+        //      int videoResourceID = _context.getResources().getIdentifier(file, "raw", _context.getPackageName());
+        //      Uri videoUri = Uri.parse("android.resource://" + _context.getPackageName() + "/" + videoResourceID);        
         Uri videoUri = Uri.parse("android.resource://" + _context.getPackageName() + "/raw/" + file);
         _videoView.setVideoURI(videoUri);
 
-        ///start our video
+        ///add video to the root view (with dummy size to minimize the stupid sliding effect) and start it
+        _rootView.addView(_videoView, getLayout(512, 512));
         _videoView.start();
     }  
 
+
+    ///sets the layout for the video
+    private static RelativeLayout.LayoutParams getLayout(int videoWidth, int videoHeight)
+    {
+        RelativeLayout.LayoutParams layoutParams = null;
+
+        ///set layout of the video view based on the layout flags
+        switch(_scaleMode)
+        {
+            case Scale_None:
+                if((videoWidth > 0) && (videoHeight > 0))
+                {
+                    layoutParams = new RelativeLayout.LayoutParams(videoWidth, videoHeight);
+                }
+                else
+                {
+                    ///fallback incase of invalid video dimensions
+                    layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);                    
+                }
+                layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+                break;
+            case Scale_Fill:
+                layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                break;
+            case Scale_FitAspect:
+                layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+                break;
+        }
+
+        return layoutParams;
+    }
 
 
     // ________________________________________________
