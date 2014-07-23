7de8388e2b1561aed934ef0dbaef31f2537e6d65
Miha Lunar <mlunar@gmail.com>
2014-07-22 23:00:30 -0400

    added ios icons and Default

---
+++ docs/examples/KrushBlokk/icons/update-icons.bat
@@ -0,0 +1,21 @@
+convert icon.png -resize 512x512 android-high-res.png
+convert icon.png -resize 72x72 android/drawable-hdpi/icon.png
+convert icon.png -resize 48x48 android/drawable-mdpi/icon.png
+convert icon.png -resize 36x36 android/drawable-ldpi/icon.png
+
+convert icon.png -resize 152x152 ios/Icon-76@2x.png
+convert icon.png -resize 144x144 ios/Icon-72@2x.png
+convert icon.png -resize 144x144 ios/Icon-144.png
+convert icon.png -resize 120x120 ios/Icon-60@2x.png
+convert icon.png -resize 114x114 ios/Icon@2x.png
+convert icon.png -resize 114x114 ios/Icon-114.png
+convert icon.png -resize 100x100 ios/Icon-Small-50@2x.png
+convert icon.png -resize 80x80 ios/Icon-Small-40@2x.png
+convert icon.png -resize 76x76 ios/Icon-76.png
+convert icon.png -resize 72x72 ios/Icon-72.png
+convert icon.png -resize 58x58 ios/Icon-Small@2x.png
+convert icon.png -resize 57x57 ios/Icon.png
+convert icon.png -resize 57x57 ios/Icon-57.png
+convert icon.png -resize 50x50 ios/Icon-Small-50.png
+convert icon.png -resize 40x40 ios/Icon-Small-40.png
+convert icon.png -resize 29x29 ios/Icon-Small.png
