030de690c3785be974b27481416538737f26468a
Miha Lunar <mlunar@gmail.com>
2014-08-05 22:48:06 -0400

    fixed bool definition in jemalloc

--- loom/vendor/jemalloc-3.4.0/include/msvc_compat/stdbool.h
+++ loom/vendor/jemalloc-3.4.0/include/msvc_compat/stdbool.h
@@ -7,8 +7,13 @@
 
 /* MSVC doesn't define _Bool or bool in C, but does have BOOL */
 /* Note this doesn't pass autoconf's test because (bool) 0.5 != true */
+/* VS2013 (_MSC_VER 1800) already defines these */
+#if defined(_MSC_VER) && _MSC_VER < 1800
 typedef BOOL _Bool;
 #define bool _Bool
+#endif
+
+typedef int bool;
 #define true 1
 #define false 0
 
