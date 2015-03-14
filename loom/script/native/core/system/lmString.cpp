cc9f868e39dc49bad6910f2874bd330115571e6b
Luke Lamothe <luke.lamothe@gmail.com>
2015-03-14 13:10:42 +0200

    Comment added to String.slice() -ve startIndex logic

--- loom/script/native/core/system/lmString.cpp
+++ loom/script/native/core/system/lmString.cpp
@@ -373,6 +373,8 @@ public:
             endIndex = svalueLength - (-(endIndex + 1));
         }
 
+        //if a -ve startIndex is provided, it means that we need to slice 
+        //that many characters from the end of the string
         if (startIndex < 0)
         {
             startIndex = svalueLength + startIndex;
