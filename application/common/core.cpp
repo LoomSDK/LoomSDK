2f7076c4889983e7daf20f44f37efd9b85ca275d
Ben Garney <ben.garney@gmail.com>
2015-03-28 16:53:22 -0700

    Tweaking windows build.

--- application/common/core.cpp
+++ application/common/core.cpp
@@ -162,66 +162,8 @@ main(int argc, char *argv[])
 #ifdef WIN32
     // When on windows, do some workarounds so our console window
     // behaves properly.
-    int    argc;
-    char** argv;
-
     char*  arg;
     int    index;
-
-    // count the arguments
-    char* command_line = (char*) malloc(strlen(lpCmdLine) + 1);
-    strcpy(command_line, lpCmdLine);
-    
-    argc = 1;
-    arg  = command_line;
-    
-    while (arg[0] != 0) {
-
-        while (arg[0] != 0 && arg[0] == ' ') {
-            arg++;
-        }
-
-        if (arg[0] != 0) {
-        
-            argc++;
-        
-            while (arg[0] != 0 && arg[0] != ' ') {
-                arg++;
-            }
-        
-        }
-    
-    }    
-    
-    // tokenize the arguments
-
-    argv = (char**)malloc(argc * sizeof(char*));
-
-    arg = command_line;
-    index = 1;
-
-    while (arg[0] != 0) {
-
-        while (arg[0] != 0 && arg[0] == ' ') {
-            arg++;
-        }
-
-        if (arg[0] != 0) {
-        
-            argv[index] = arg;
-            index++;
-        
-            while (arg[0] != 0 && arg[0] != ' ') {
-                arg++;
-            }
-        
-            if (arg[0] != 0) {
-                arg[0] = 0;    
-                arg++;
-            }
-        
-        }
-    }
 
     // put the program name into argv[0]
     char filename[_MAX_PATH];
@@ -267,12 +209,9 @@ main(int argc, char *argv[])
 
         LS::Process::consoleAttached = true;
     }
+#endif
 
     LSLuaState::initCommandLine(argc, (const char**) argv);
-    
-    free(argv);
-    free(command_line);
-#endif
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
