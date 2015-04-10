5cae1edd9fc0f19f7d1566177386a3a0d1e24c75
Miha Lunar <mlunar@gmail.com>
2015-04-10 04:05:52 +0200

    Added some comments to IME stuff in lmSDL.

--- loom/engine/bindings/sdl/lmSDL.h
+++ loom/engine/bindings/sdl/lmSDL.h
@@ -52,38 +52,82 @@ class IMEDelegate
 {
 public:
 
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(DidAttachWithIME);
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(DidDetachWithIME);
+
+    /** Invoked when there is new text to be inserted. */
     LOOM_DELEGATE(InsertText);
+
+    /** Invoked when the text should be deleted backwards (backspace). */
     LOOM_DELEGATE(DeleteBackward);
+
+    /** Invoked when there is a new IME composition candidate to be shown */
     LOOM_DELEGATE(ShowComposition);
+
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(KeyboardWillShow);
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(KeyboardDidShow);
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(KeyboardWillHide);
+    /** Deprecated, not used right now. */
     LOOM_DELEGATE(KeyboardDidHide);
 
+    /** Deprecated, not used right now. */
     bool     canAttachWithIME;
+    /** Deprecated, not used right now. */
     bool     canDetachWithIME;
+    /** Deprecated, not used right now. */
     utString contentText;
 
     IMEDelegate();
     ~IMEDelegate();
 
+    /** Deprecated, not used right now. */
     virtual void didAttachWithIME();
+    /** Deprecated, not used right now. */
     virtual void didDetachWithIME();
 
+    /** Invoke the insert text delegate. */
     virtual void insertText(const char *text, int len);
+
+    /** Invoke the delete backward delegate. */
     virtual void deleteBackward();
+
+    /** Invoke the show composition delegate. */
     virtual void showComposition(const char *text, int len, int start, int length);
 
+    /** Deprecated, not used right now. */
     virtual const char *getContentText();
 
+    /** Deprecated, not used right now. */
     virtual void keyboardWillShow(IMEKeyboardNotificationInfo& info);
+    /** Deprecated, not used right now. */
     virtual void keyboardDidShow(IMEKeyboardNotificationInfo& info);
+    /** Deprecated, not used right now. */
     virtual void keyboardWillHide(IMEKeyboardNotificationInfo& info);
+    /** Deprecated, not used right now. */
     virtual void keyboardDidHide(IMEKeyboardNotificationInfo& info);
 
+    /**
+     * Set the IME text input rectangle in device points.
+     * This is used for positioning the IME interfaces or
+     * moving the rendering to not get covered by the
+     * soft keyboard on mobile devices.
+     */
     void setTextInputRect(Loom2D::Rectangle rect);
+
+    /**
+     * Begin text input and open a soft keyboard if available.
+     * This begins the delegate invocation of insertText, showComposition, etc.
+     */
     bool attachWithIME(int type = 0);
+
+    /**
+     * End text input and close the soft keyboard if available and opened.
+     * This ends the delegate invocation of insertText, showComposition, etc.
+     */
     bool detachWithIME();
 };
