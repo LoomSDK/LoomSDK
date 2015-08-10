e4ad03b4701cb56328ccd0b2f9a783d241162ebe
Jure Gregorin <jgregorin@gmail.com>
2015-08-10 16:27:33 +0200

    Simple GL state managment implementation
    This removes some  "cleanup" that the two renderers were doing, so
    they can reuse the state they set up before.
    
    Additionally, some whitespace errors are fixed.

---
+++ loom/graphics/gfxStateManager.h
@@ -0,0 +1,43 @@
+/*
+ * ===========================================================================
+ * Loom SDK
+ * Copyright 2011, 2012, 2013
+ * The Game Engine Company, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===========================================================================
+ */
+
+#pragma once
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
+enum Graphics_GLState {
+    GFX_OPENGL_STATE_NANOVG = 0,
+    GFX_OPENGL_STATE_QUAD   = 1,
+    GFX_OPENGL_STATE_MAX    = 2,
+};
+
+bool Graphics_IsGLStateValid(enum Graphics_GLState state);
+void Graphics_InvalidateGLState(enum Graphics_GLState state);
+void Graphics_SetCurrentGLState(enum Graphics_GLState state);
+
+#ifdef __cplusplus
+};
+#endif
