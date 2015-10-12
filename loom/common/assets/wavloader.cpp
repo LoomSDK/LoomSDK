7559261ea36bda37f9e2781d835ce68d3da5ed81
Jure Gregorin <jgregorin@gmail.com>
2015-10-12 17:11:52 +0200

    Added headers and docs

--- loom/common/assets/wavloader.cpp
+++ loom/common/assets/wavloader.cpp
@@ -1,3 +1,23 @@
+/*
+ * ===========================================================================
+ * Loom SDK
+ * Copyright 2011, 2012, 2013
+ * The Game Engine Company, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===========================================================================
+ */
+
 #include <string.h>
 #include "wavloader.h"
 #include "loom/common/core/log.h"
@@ -28,9 +48,9 @@ typedef struct
 } fmt_chunk;
 
 bool load_wav(const uint8_t* inData,
-                  const int32_t inDataLen,
-                  uint8_t* outData,
-                  wav_info* outInfo)
+              const int32_t inDataLen,
+              uint8_t* outData,
+              wav_info* outInfo)
 {
     if (inData == NULL)
     {
