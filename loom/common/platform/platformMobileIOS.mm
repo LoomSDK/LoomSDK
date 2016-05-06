72ebaf1af3f3809f204f79828a156a9fc0f170c3
Tadej Osolnik <tadej.osolnik@gmail.com>
2016-05-06 13:13:26 +0200

    Fixed "isSensorEnabled" to actually check if sensor is enabled instead of only if it's supported, added some comments

--- loom/common/platform/platformMobileIOS.mm
+++ loom/common/platform/platformMobileIOS.mm
@@ -221,16 +221,18 @@ void handleGenericEvent(void *userdata, const char *type, const char *payload);
     return locString;
 }
 
+//returns true if accelerometer is supported on device
 -(BOOL)isAccelerometerAvailable {
     return self.motionManager.accelerometerAvailable;
 }
 
+//returns true if accelerometer is active (it happens when startAccelerometerUpdates is called on motionManager)
 -(BOOL)isAccelerometerActive {
     return self.motionManager.accelerometerActive;
 }
 
 -(void)enableAccelerometer:(SensorTripleChangedCallback) gTripleChangedCallback {
-    if (self.isAccelerometerActive) return;
+    if (self.isAccelerometerActive || !self.isAccelerometerAvailable) return;
     
     [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
         if (gTripleChangedCallback != NULL) {
@@ -491,7 +493,7 @@ bool platform_isSensorEnabled(int sensor)
     switch (sensor)
     {
         case 0:
-            enabled = [mobileiOS isAccelerometerAvailable];
+            enabled = [mobileiOS isAccelerometerActive];
             break;
     }
     
