fcc6171af6afdc227bb4462dcad4d2288706019a
Miha Lunar <mlunar@gmail.com>
2016-06-09 20:13:25 +0200

    Fixed SDL activity and cleaned up some of the integration, Android seems to work

--- loom/engine/SDL2/platform/android/java/jni/src/Android.mk
+++ loom/engine/SDL2/platform/android/java/jni/src/Android.mk
@@ -10,7 +10,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c
-	
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
